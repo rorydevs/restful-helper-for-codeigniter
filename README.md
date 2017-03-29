c341dbc3c66fd3ab60adc58ee5226236d7c9a798
Rory Molyneux <rorymolyneux@gmail.com>
2017-03-29 18:33:34 +0200

    Initial commit. This library has served me well for nearly 3 years!

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# restful-helper-for-codeigniter
-A small helper library for assisting with creating RESTful services with Codeigniter
+# Restful Helper For Codeigniter
+A simple helper library for assisting with creating RESTful services with Codeigniter
+
+## What is this?
+Built out of necessity for working with RESTful frameworks like Backbonejs, I built this library to assist with basic REST handling inside Codeigniter. It's not robust, but it gets the job done for basic REST handling.
+
+## Usage
+* Clone or download this repo
+* Copy the application folder into your Codeigniter project directory
+* Load the restful helper in your controller with
+> $this->load->helper('restful');
+* Or autoload the helper file in your autoload.php
+
+### Functions
+#### get_request_type()
+Gets the current request type so that you can determine whether the service is requesting a GET, POST, PUT or DELETE
+
+#### allowed_request_types($allowed_requests)
+Allows you to gate away specific request types. If your endpoint is GET only, you can restrict it using
+> allowed_request_types('get');
+This will reject all server requests but GET with a 403 response
+
+#### return_json($obj, $status = 200)
+This method will return your data as a JSON string with a 200 HTTP status code, unless otherwise specified. Useful for if you want to standardise how you send data back to the application.
+
+#### get_vars()
+Returns a PHP Object of any/all POST, PUT or GET values sent to your endpoint.
---
+++ application/helpers/restful_helper.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * RESTful Helper
+ * 
+ * Helper functions for RESTful web service creation
+ * @author Rory Molyneux <rorymolyneux@gmail.com>
+ * @date 2017-03-29
+ * @version 1.0
+ */
+
+/**
+ * Get Request Type
+ * 
+ * Gets the type of server request: GET, POST, PUT, DELETE
+ * @return string
+ */
+function get_request_type() {
+    // Loads up the current CI instance
+    $ci =& get_instance();
+    // Gets the current request method
+    $method = $ci->input->server('REQUEST_METHOD');
+    
+    return strtolower($method);
+}
+
+/**
+ * Allowed Request Types
+ * 
+ * Simple security check. Allows only the permitted request types through.
+ * All HTTP request types should be lowercase
+ * 
+ * @param array $allowed_requests (lowercase)
+ */
+function allowed_request_types($allowed_requests) {
+    
+    // Get request type
+    $request = get_request_type();
+    
+    if(!is_array($allowed_requests)) {
+        $allowed_requests = array($allowed_requests);
+    }
+    
+    // Convert request types to lowercase
+    foreach($allowed_requests as $k => $req) {
+        $allowed_requests[$k] = strtolower($req);
+    }
+    
+    if(!in_array($request, $allowed_requests)) {
+        // Request is denied, send rejection message
+        $return = array('status' => 0,
+                        'message' => 'The request type is not allowed');
+        return_json($return, 403);
+        exit;
+    } else {
+        return $request;
+    }
+}
+
+/**
+ * Return JSON
+ * 
+ * Returns the object in JSON form to be consumed by an endpoint
+ * @param object/array $obj
+ * @return string
+ */
+function return_json($obj, $status = 200) {
+    
+    http_response_code($status);
+    
+    if(is_null($obj)) {
+        $obj = array();
+    }
+    
+    echo json_encode($obj);
+    exit;
+    
+}
+
+/**
+ * Get Post Vars
+ * 
+ * Gets the post variables transmitted via
+ * JSON
+ * 
+ * @return mixed
+ */
+function get_vars() {
+    
+    $ci =& get_instance();
+    
+    $post = $ci->input->post();
+    $get = $ci->input->get();
+    $phpinput = (Array)json_decode(file_get_contents('php://input'));
+    
+    if(is_null($post) || !$post) {
+        $post = array();
+    }
+    
+    if(is_null($get) || !$get) {
+        $get = array();
+    }
+    
+    $vars = array_merge($post, $get, $phpinput);
+    
+    return (object) $vars;
+}
